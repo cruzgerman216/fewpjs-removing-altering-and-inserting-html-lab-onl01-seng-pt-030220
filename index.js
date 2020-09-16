d2d4df05dea99ea3dffcde9b85832dd78500dad0
German Cruz <cruzgerman216@yahoo.com>
2020-09-16 18:01:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ let main = document.getElementById("main");
 main.remove();
 
 let newHeader = document.createElement('h1#victory');
-document.body.appendChild(newHeader)
+document.body.appendChild(newHeader);
